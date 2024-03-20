fc82b8a50e8f4607711d5ebd399351992316ddd1
DenserMeerkat <mragulmanoharan@gmail.com>
2024-03-21 02:53:05 +0530

    wip: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
-import {
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
-} from "@nextui-org/navbar";
+import { useState, useEffect, useCallback } from "react";
 import {
   Modal,
   ModalContent,
@@ -17,115 +9,241 @@ import {
   useDisclosure,
 } from "@nextui-org/modal";
 import { Button } from "@nextui-org/button";
-import { Tabs, Tab } from "@nextui-org/tabs";
 import { useMediaQuery } from "@react-hook/media-query";
 import NextLink from "next/link";
 import clsx from "clsx";
 import { siteConfig } from "@/config/site";
 
 import { ThemeSwitch } from "@/components/theme-switch";
-import { HandMetal, UserCircle2 } from "lucide-react";
-import { DotsVerticalIcon } from "@radix-ui/react-icons";
+import { Ellipsis, HandMetal, UserCircle2 } from "lucide-react";
+import { ClassNameProp } from "@/types";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 export const Navbar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("l") || "";
   const [isDomLoaded, setIsDomLoaded] = useState(false);
-  const isMobile = useMediaQuery("(max-width: 768px)");
-  useEffect(() => {
-    setIsDomLoaded(true);
-  }, []);
-  if (!isDomLoaded) return null;
-  if (isMobile)
-    return (
-      <NextUINavbar
-        maxWidth="full"
-        position="sticky"
-        isBordered
-        isBlurred={false}
-        className="md:hidden h-fit"
-      >
-        <NavbarBrand>
-          <SignlingoTitle />
-        </NavbarBrand>
+  const isMobile = useMediaQuery("(max-width: 625px)");
+  const isTablet = useMediaQuery("(max-width: 1024px)");
 
-        <NavbarContent className="basis-1 pl-4" justify="end">
-          <ThemeSwitch />
-          <Profile />
-        </NavbarContent>
-      </NextUINavbar>
-    );
-  else
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  useEffect(() => {
+    console.log(search);
+    setIsDomLoaded(true);
+    if (search == "") {
+      router.push(pathname + "?" + createQueryString("l", "alphabets"));
+    }
+  }, []);
+
+  if (!isDomLoaded) return null;
+  if (isMobile) {
     return (
-      <header className="hidden md:block min-w-[250px] w-full max-w-[300px] h-screen fixed border-r-2 border-border">
+      <nav className="w-screen fixed border-t-2 border-border bottom-0">
+        <div className="flex justify-center gap-4 items-center py-4 px-8">
+          <ProfileTile />
+          <ThemeSwitch />
+          <MoreActionsTile />
+        </div>
+      </nav>
+    );
+  }
+  if (isTablet)
+    return (
+      <nav className="w-[80px] h-screen fixed border-r-2 border-border">
         <div className="flex flex-col h-full justify-between p-4 pb-5">
           <SignlingoTitle />
-          <div className="flex w-full gap-3 justify-between">
-            <ThemeSwitch />
-            <Profile />
-            <MoreActions />
+          <div className="flex flex-col gap-4 items-center">
+            <CharacterTile
+              iconCharacters="Ab"
+              label="Alphabets"
+              isSelected={search == "alphabets"}
+              onPress={() => {
+                router.push(
+                  pathname + "?" + createQueryString("l", "alphabets")
+                );
+              }}
+            />
+            <CharacterTile
+              iconCharacters="12"
+              label="Numbers"
+              isSelected={search == "numbers"}
+              onPress={() => {
+                router.push(pathname + "?" + createQueryString("l", "numbers"));
+              }}
+            />
+          </div>
+          <div className="flex flex-col lg:flex-row w-full gap-3 justify-between items-center">
+            <div className="order-2">
+              <ThemeSwitch />
+            </div>
+            <div className="order-1">
+              <ProfileTile />
+            </div>
+            <div className="order-3">
+              <MoreActionsTile />
+            </div>
           </div>
         </div>
-      </header>
+      </nav>
     );
+  return (
+    <nav className="min-w-[250px] w-full max-w-[280px] h-screen fixed border-r-2 border-border">
+      <div className="flex flex-col h-full justify-between p-4 pb-5">
+        <SignlingoTitle />
+        <div className="flex flex-col gap-4 px-4 items-center">
+          <CharacterTile
+            iconCharacters="Abc"
+            label="Alphabets"
+            isSelected={search == "alphabets"}
+            onPress={() => {
+              router.push(pathname + "?" + createQueryString("l", "alphabets"));
+            }}
+          />
+          <CharacterTile
+            iconCharacters="123"
+            label="Numbers"
+            isSelected={search == "numbers"}
+            onPress={() => {
+              router.push(pathname + "?" + createQueryString("l", "numbers"));
+            }}
+          />
+        </div>
+        <div className="flex flex-col lg:flex-row w-full gap-3 justify-between items-center">
+          <ThemeSwitch />
+          <ProfileTile />
+          <MoreActionsTile />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-const SignlingoTitle = () => {
+const SignlingoTitle = ({ className }: ClassNameProp) => {
   return (
     <NextLink
-      className="flex justify-start md:justify-center items-center gap-2.5 text-primary md:pr-4"
-      href="/"
+      className={clsx(
+        "flex justify-center items-center gap-2.5 text-primary",
+        className
+      )}
+      href="/?l=alphabets"
     >
-      <HandMetal fill="currentColor" stroke="none" />
-      <span className="text-primary font-bold tracking-widest text-2xl">
+      <HandMetal fill="currentColor" className="h-8 w-8" stroke="none" />
+      <span className="hidden lg:block text-primary font-bold tracking-widest text-2xl">
         {siteConfig.name.toLocaleLowerCase()}
       </span>
     </NextLink>
   );
 };
 
-const Profile = () => {
-  const isMobile = useMediaQuery("(max-width: 768px)");
+const ProfileTile = ({ className }: ClassNameProp) => {
+  const isMobile = useMediaQuery("(max-width: 625px)");
+  const isTablet = useMediaQuery("(max-width: 1024px)");
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      {isMobile ? (
-        <Button
-          isIconOnly={true}
-          onPress={onOpen}
-          color="primary"
-          variant="flat"
-          className="text-primary-foreground dark:text-primary"
-        >
+      <Button
+        isIconOnly={isTablet ? true : false}
+        onPress={onOpen}
+        color={
+          isTablet
+            ? isMobile
+              ? isOpen
+                ? "primary"
+                : "default"
+              : "primary"
+            : "primary"
+        }
+        variant="flat"
+        startContent={isTablet ? null : <UserCircle2 size={22} />}
+        className={clsx(
+          "text-primary-foreground dark:text-primary lg:w-full",
+          className
+        )}
+      >
+        {isTablet ? (
           <UserCircle2 size={22} />
-        </Button>
-      ) : (
-        <Button
-          onPress={onOpen}
-          color="primary"
-          variant="flat"
-          startContent={<UserCircle2 size={22} />}
-          className="w-full text-primary-foreground dark:text-primary"
-        >
-          <span className="ml-1 text-lg font-medium tracking-widest">
+        ) : (
+          <span className="hidden lg:block ml-1 text-lg font-medium tracking-widest">
             Profile
           </span>
-        </Button>
-      )}
+        )}
+      </Button>
     </>
   );
 };
 
-const MoreActions = () => {
+const MoreActionsTile = ({ className }: ClassNameProp) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className="flex flex-col gap-2">
+    <Button
+      isIconOnly={true}
+      onPress={onOpen}
+      variant="flat"
+      className={clsx("dark:border-zinc-800 dark:bg-zinc-900", className)}
+    >
+      <Ellipsis className="dark:text-primary h-5 w-5" />
+    </Button>
+  );
+};
+
+interface CharacterTileProps extends ClassNameProp {
+  iconCharacters: string;
+  label: string;
+  isSelected: boolean;
+  onPress: () => void;
+}
+
+const CharacterTile = (props: CharacterTileProps) => {
+  const { iconCharacters, label, isSelected, onPress, className } = props;
+  const isTablet = useMediaQuery("(max-width: 1024px)");
+
+  const Icon = () => {
+    return (
+      <div className="flex justify-center items-center">
+        <span
+          className={clsx(
+            "text-primary-foreground dark:text-primary text-lg lg:text-xl font-semibold lg:w-12 lg:bg-primary/10 lgpx-1.5 lg:py-.5 lg:rounded-lg"
+          )}
+        >
+          {iconCharacters}
+        </span>
+      </div>
+    );
+  };
+
+  return (
+    <>
       <Button
-        isIconOnly
-        onPress={onOpen}
-        variant="flat"
-        className="dark:border-zinc-800 dark:bg-zinc-900 w-full"
+        isIconOnly={isTablet ? true : false}
+        onPress={onPress}
+        color="primary"
+        size="lg"
+        variant={isSelected ? "bordered" : "light"}
+        startContent={isTablet ? null : <Icon />}
+        className={clsx(
+          "flex items-center lg:pr-4 lg:pl-1.5 gap-2.5 text-primary-foreground dark:text-primary lg:w-full",
+          className
+        )}
       >
-        <DotsVerticalIcon />
+        {isTablet ? (
+          <Icon />
+        ) : (
+          <span className="text-lg font-medium tracking-widest w-28 text-start">
+            {label}
+          </span>
+        )}
       </Button>
-    </div>
+    </>
   );
 };
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -7,6 +7,7 @@ import { useIsSSR } from "@react-aria/ssr";
 import { Button } from "@nextui-org/button";
 
 import { SunMedium, MoonStar } from "lucide-react";
+import clsx from "clsx";
 
 export interface ThemeSwitchProps {
   className?: string;
@@ -29,7 +30,12 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({ className }) => {
   });
 
   return (
-    <Button isIconOnly variant="flat" onClick={onChange}>
+    <Button
+      isIconOnly
+      variant="flat"
+      onClick={onChange}
+      className={clsx(className)}
+    >
       {!isSelected || isSSR ? (
         <MoonStar className="text-primary" size={22} />
       ) : (
