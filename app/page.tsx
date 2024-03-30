3c97b1ab98783fcfbc3a85db09e31a45d90fb0fd
DenserMeerkat <mragulmanoharan@gmail.com>
2024-03-31 00:46:01 +0530

    fix: loading indicator

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,12 @@ export default function Home() {
 const LoadingScreen = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-background">
-      <CircularProgress color="primary" />
+      <CircularProgress
+        aria-label="Loading..."
+        classNames={{
+          svg: "w-20 h-20 drop-shadow-md",
+        }}
+      />
     </div>
   );
 };
